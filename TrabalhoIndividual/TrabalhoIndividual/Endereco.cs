8393ac037c42b8649adec94cf1dc776bdc93e26d
Camille Ouriques <mille.ouriques@gmail.com>
2021-07-16 14:46:38 -0300

    Prontinho

---
+++ TrabalhoIndividual/TrabalhoIndividual/Endereco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoIndividual
+{
+    public class Endereco
+    {
+        string rua = String.Empty;
+        int numero = 0;
+        string complemento = String.Empty;
+        string bairro = String.Empty;
+        string cidade = String.Empty;
+    }
+}
---
+++ TrabalhoIndividual/TrabalhoIndividual/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoIndividual
+{
+    public class Pessoa
+    {
+        string nome = String.Empty;
+
+        protected DateTime nascimento;
+
+        string endereco = String.Empty;
+
+    }
+}
